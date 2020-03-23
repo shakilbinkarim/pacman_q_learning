517e1aa639b7b6b1a0ac9f02e7b8d1d9ad92b4ec
Samin Bin Karim <saminbinkarim@gmail.com>
2020-03-24 01:28:41 +0600

    Random Pacman Added

--- pacman_ql/Assets/Scripts/Monobehaviours/GridWorld.cs
+++ pacman_ql/Assets/Scripts/Monobehaviours/GridWorld.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 public class GridWorld : MonoBehaviour
 {
 	public List<Wall> Walls { get => _walls; }
-	public WorldStaticEntity[,] WorldStaticEntities{ get => _wordlStaticEntities;}
+	public WorldStaticEntity[,] WorldStaticEntities { get => _wordlStaticEntities; }
 	#region SerializedFields
 
 	[SerializeField] private Texture2D map;
 	[SerializeField] private Texture2D pacmanTexture;
 	[SerializeField] private Texture2D wallTexture;
-	[SerializeField] private Texture2D foodTexture; 
-	[Header("Ghosts")] [Space(10)]
+	[SerializeField] private Texture2D foodTexture;
+	[Header("Ghosts")]
+	[Space(10)]
 	[SerializeField] private Texture2D[] ghostTextures;
 	[SerializeField] private int numberOfGhosts;
 	#endregion
@@ -21,32 +22,35 @@ public class GridWorld : MonoBehaviour
 	#region Sprites
 
 	private Sprite _wallSprite;
-    private Sprite[] _ghostSprites;
-    private Sprite _pacmanSprite;
+	private Sprite[] _ghostSprites;
+	private Sprite _pacmanSprite;
 	private Sprite _foodSprite;
 
 	#endregion
 
 	#region WorldEntititesLists
 
-	
+
 	private List<Wall> _walls;
-    private List<Ghost> _ghosts;
-    private List<Food> _foods;
+	private List<Ghost> _ghosts;
+	private List<Food> _foods;
+	private Pacman _pacman;
 
 	#endregion
 
 	private WorldStaticEntity[,] _wordlStaticEntities;
 
-    private void Start()
-    {
-	    InitLists();
+	private void Start()
+	{
+		InitLists();
 		InitGrid();
-	    InitSprites();
-	    CreateGridWorld();
+		InitSprites();
+		CreateGridWorld();
 		CreateGhosts();
+		CreatePacman();
 		Movement.GridWorld = this;
-    }
+	}
+
 
 	private void InitGrid()
 	{
@@ -56,30 +60,36 @@ public class GridWorld : MonoBehaviour
 	private void Update()
 	{
 		UpdateGhosts();
+		UpdatePacmac();
+	}
+
+	private void UpdatePacmac()
+	{
+		_pacman.gameObject.transform.localPosition = new Vector2(_pacman.Coordinates.X, _pacman.Coordinates.Y);
 	}
 
 	private void UpdateGhosts()
 	{
 		foreach (Ghost ghost in _ghosts)
 		{
-			ghost.gameObject.transform.localPosition = new Vector2(ghost.Coordinates.X, ghost.Coordinates.Y); 
+			ghost.gameObject.transform.localPosition = new Vector2(ghost.Coordinates.X, ghost.Coordinates.Y);
 		}
 	}
 
 	private void InitLists()
-    {
-	    _walls = new List<Wall>();
-	    _ghosts = new List<Ghost>();
-	    _foods = new List<Food>();
-    }
+	{
+		_walls = new List<Wall>();
+		_ghosts = new List<Ghost>();
+		_foods = new List<Food>();
+	}
 
-    private void InitSprites()
-    {
-		_foodSprite = SpriteCreator.CreateSprite(foodTexture, foodTexture.width*2, Vector2.zero);
+	private void InitSprites()
+	{
+		_foodSprite = SpriteCreator.CreateSprite(foodTexture, foodTexture.width * 2, Vector2.zero);
 		_wallSprite = SpriteCreator.CreateSprite(wallTexture, wallTexture.width, Vector2.zero);
-	    _pacmanSprite = SpriteCreator.CreateSprite(pacmanTexture, pacmanTexture.width, Vector2.zero);
+		_pacmanSprite = SpriteCreator.CreateSprite(pacmanTexture, pacmanTexture.width, Vector2.zero);
 		InitGhostSprites();
-    }
+	}
 
 	private void InitGhostSprites()
 	{
@@ -87,29 +97,46 @@ public class GridWorld : MonoBehaviour
 		for (int i = 0; i < numberOfGhosts; i++)
 		{
 			if (ghostTextures.Length <= 0) return;
-			Texture2D  ghostTexture = ghostTextures[i%ghostTextures.Length];
+			Texture2D ghostTexture = ghostTextures[i % ghostTextures.Length];
 			_ghostSprites[i] = SpriteCreator.CreateSprite(ghostTexture, ghostTexture.width, Vector2.zero);
 		}
 	}
 
 	private void CreateGridWorld()
-    {
-	    for (int i = 0; i < map.width; i++)
-	    {
-		    for (int j = 0; j < map.height; j++) CreateBlock(i, j);
-	    }
+	{
+		for (int i = 0; i < map.width; i++)
+		{
+			for (int j = 0; j < map.height; j++) CreateBlock(i, j);
+		}
 	}
 
-    private void CreateBlock(int x, int y)
-    {
-	    Color pixelColor = map.GetPixel(x, y);
-	    if (pixelColor == MapColors.WallColor) CreateWall(x, y);  
-	    else if (pixelColor == MapColors.SmallFoodColor) CreateSmallFood(x, y);
-	    else if (pixelColor == MapColors.BigFoodColor) CreateBigFood(x, y);
-    }
-
-    private void CreateBigFood(int x, int y)
-    {
+	private void CreateBlock(int x, int y)
+	{
+		Color pixelColor = map.GetPixel(x, y);
+		if (pixelColor == MapColors.WallColor) CreateWall(x, y);
+		else if (pixelColor == MapColors.SmallFoodColor) CreateSmallFood(x, y);
+		else if (pixelColor == MapColors.BigFoodColor) CreateBigFood(x, y);
+	}
+	private void CreateWall(int x, int y)
+	{
+		GameObject wallGameObj = new GameObject($"wall_{x}_{y}");
+		SpriteRenderer spriteRenderer = wallGameObj.AddComponent<SpriteRenderer>();
+		spriteRenderer.sprite = _wallSprite;
+		spriteRenderer.sortingLayerID = SortingLayer.NameToID(SortingLayerNames.Wall);
+		spriteRenderer.color = Color.white;
+		Wall wall = wallGameObj.AddComponent<Wall>();
+		wall.Coordinates = new Coordinates(x, y);
+		WorldStaticEntity worldEntity = wallGameObj.AddComponent<WorldStaticEntity>();
+		worldEntity.Type = WorldStaticEntityType.Wall;
+		_wordlStaticEntities[x, y] = worldEntity;
+		_walls.Add(wall);
+		wallGameObj.transform.SetParent(gameObject.transform);
+		wallGameObj.transform.localPosition = new Vector3(wall.Coordinates.X, wall.Coordinates.Y, gameObject.transform.position.z);
+	}
+	
+	#region Create Food
+	private void CreateBigFood(int x, int y)
+	{
 		GameObject bigFoodGameObj = new GameObject($"big_food_{x}_{y}");
 		SpriteRenderer spriteRenderer = bigFoodGameObj.AddComponent<SpriteRenderer>();
 		spriteRenderer.sprite = _foodSprite;
@@ -126,8 +153,8 @@ public class GridWorld : MonoBehaviour
 		bigFoodGameObj.transform.localPosition = new Vector3(food.Coordinates.X + 0.25f, food.Coordinates.Y + 0.25f, gameObject.transform.position.z);
 	}
 
-    private void CreateSmallFood(int x, int y)
-    {
+	private void CreateSmallFood(int x, int y)
+	{
 		GameObject smallFoodGameObj = new GameObject($"small_food_{x}_{y}");
 		SpriteRenderer spriteRenderer = smallFoodGameObj.AddComponent<SpriteRenderer>();
 		spriteRenderer.sprite = _foodSprite;
@@ -141,34 +168,20 @@ public class GridWorld : MonoBehaviour
 		food.Type = FoodType.Small;
 		_foods.Add(food);
 		smallFoodGameObj.transform.SetParent(gameObject.transform);
-		smallFoodGameObj.transform.localPosition = new Vector3(food.Coordinates.X + 0.25f, food.Coordinates.Y +0.25f, gameObject.transform.position.z);
+		smallFoodGameObj.transform.localPosition = new Vector3(food.Coordinates.X + 0.25f, food.Coordinates.Y + 0.25f, gameObject.transform.position.z);
 	}
+	#endregion
 
-    private void CreateWall(int x, int y)
-    {
-	    GameObject wallGameObj = new GameObject($"wall_{x}_{y}");
-	    SpriteRenderer spriteRenderer = wallGameObj.AddComponent<SpriteRenderer>();
-	    spriteRenderer.sprite = _wallSprite;
-	    spriteRenderer.sortingLayerID = SortingLayer.NameToID(SortingLayerNames.Wall);
-	    spriteRenderer.color = Color.white;
-	    Wall wall = wallGameObj.AddComponent<Wall>();
-	    wall.Coordinates = new Coordinates(x, y);
-		WorldStaticEntity worldEntity = wallGameObj.AddComponent<WorldStaticEntity>();
-		worldEntity.Type = WorldStaticEntityType.Wall;
-		_wordlStaticEntities[x, y] = worldEntity;
-		_walls.Add(wall);
-	    wallGameObj.transform.SetParent(gameObject.transform);
-	    wallGameObj.transform.localPosition = new Vector3(wall.Coordinates.X, wall.Coordinates.Y, gameObject.transform.position.z);
-    }
-
-	private void CreateGhosts() {
+	#region Create Ghosts
+	private void CreateGhosts()
+	{
 		for (int i = 0; i < numberOfGhosts; i++)
 		{
-			Coordinates coordinates =  _foods[UnityEngine.Random.Range(0, _foods.Count)].Coordinates;
+			Coordinates coordinates = _foods[UnityEngine.Random.Range(0, _foods.Count)].Coordinates;
 			CreateGhost(coordinates, i);
 		}
 	}
-	
+
 	private void CreateGhost(Coordinates coordinates, int spriteIndex)
 	{
 		GameObject ghostGameObj = new GameObject($"ghost_{spriteIndex}");
@@ -182,5 +195,38 @@ public class GridWorld : MonoBehaviour
 		ghostGameObj.transform.SetParent(gameObject.transform);
 		ghostGameObj.transform.localPosition = new Vector3(ghost.Coordinates.X, ghost.Coordinates.Y, gameObject.transform.position.z);
 	}
-    
+	#endregion
+
+	#region Create Pacman
+	private void CreatePacman()
+	{
+		GameObject pacmanGameObj = new GameObject($"pacman");
+		SpriteRenderer spriteRenderer = pacmanGameObj.AddComponent<SpriteRenderer>();
+		spriteRenderer.sprite = _pacmanSprite;
+		spriteRenderer.sortingLayerID = SortingLayer.NameToID(SortingLayerNames.Pacman);
+		spriteRenderer.color = Color.white;
+		Pacman pacman = pacmanGameObj.AddComponent<Pacman>();
+		_pacman = pacman;
+		Coordinates coordinates = CalcPacmanCoordinate();
+		pacman.Coordinates = coordinates;
+		pacmanGameObj.transform.SetParent(gameObject.transform);
+		pacmanGameObj.transform.localPosition = new Vector3(pacman.Coordinates.X, pacman.Coordinates.Y, gameObject.transform.position.z);
+	}
+
+	private Coordinates CalcPacmanCoordinate()
+	{
+		Coordinates coordinates = new Coordinates(0, 0);
+		bool loop = true;
+		while (loop)
+		{
+			loop = false;
+			coordinates = _foods[UnityEngine.Random.Range(0, _foods.Count)].Coordinates;
+			foreach (Ghost ghost in _ghosts)
+			{
+				if (ghost.Coordinates == coordinates) loop = true;
+			}
+		}
+		return coordinates;
+	}
+	#endregion
 }
--- pacman_ql/Assets/Scripts/Monobehaviours/WorldEntities/Pacman.cs
+++ pacman_ql/Assets/Scripts/Monobehaviours/WorldEntities/Pacman.cs
@@ -1,7 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Pacman : MonoBehaviour
 {
-    public Coordinates Coordinates { get; set; }
-    
+	#region Properties
+
+	public Coordinates Coordinates { get; set; }
+
+	#endregion
+
+	private List<Moves> _moves;
+	private float _elapsedTime;
+	private float _waitTime = 1.0f; // TODO: change from magic numer
+
+	private void Start()
+	{
+		_moves = new List<Moves>();
+		_elapsedTime = 0.0f;
+	}
+
+	private void Update()
+	{
+		if (_elapsedTime < _waitTime)
+		{
+			_elapsedTime += Time.deltaTime;
+			return;
+		}
+		Move();
+	}
+
+	public void Move()
+	{
+		_moves.Clear();
+		_elapsedTime = 0.0f;
+		_moves = Movement.GetValidMoves(Coordinates);
+		Moves selectedMove = _moves[Random.Range(0, _moves.Count)];
+		Coordinates.Move(selectedMove);
+	}
+
 }
